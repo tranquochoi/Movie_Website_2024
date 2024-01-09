dd6fd80b22b723ed79b55d93f82b3b1eee8d572b
DESKTOP-UF62SM0\WalterWhite <boemhut113@gmail.com>
2024-01-09 23:12:55 +0700

    Tạo vùng làm việc, Duy detail, Tín search, Thành Hội home.

---
+++ mymoviewebsite/components/landing_page/homePage.tsx
@@ -0,0 +1,11 @@
+import { Box, Container, Stack } from "@mui/material";
+
+const Homepage = () => {
+  return (
+    <Container>
+      <Box></Box>
+    </Container>
+  );
+};
+
+export default Homepage;
