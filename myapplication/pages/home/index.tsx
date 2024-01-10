75838fa7f8ed9f09da9a9e2f87c3ac73cb4a9c8e
kazuo <92442628+Kazuo01@users.noreply.github.com>
2024-01-11 00:48:44 +0700

    home index update

--- myapplication/pages/home/index.tsx
+++ myapplication/pages/home/index.tsx
@@ -1,8 +1,17 @@
-import { Box, Card, CardContent, CardMedia, CircularProgress, Grid, Link, Typography } from '@mui/material';
-import useSWR from 'swr';
-import { NextPageWithLayout } from '../_app';
-import Layout from '@/components/landing_page/layout';
-import axios from 'axios';
+import type { ReactElement } from "react";
+import Layout from "@/components/landing_page/layout";
+import {
+    Box,
+    Card,
+    CardContent,
+    CardMedia,
+    CircularProgress,
+    Link,
+    Typography,
+} from "@mui/material";
+import axios from "axios";
+import useSWR from "swr";
+import { NextPageWithLayout } from "../_app";
 
 interface MovieList {
     results: Movie[];
@@ -15,49 +24,72 @@ interface Movie {
 }
 
 const HomeDetail: NextPageWithLayout = () => {
-    const fetcher = (url: string) => axios.get(url).then((response) => response.data);
-    const { data, error } = useSWR<MovieList>('/movie/upcoming', fetcher);
-
-    if (!data) {
-        return <CircularProgress />;
-    }
-
-    if (error) {
-        return <Typography>Error loading data</Typography>;
-    }
+    const fetcher = (url: string) =>
+        axios.get(url).then((response) => response.data);
+    const { data, isLoading, error } = useSWR<MovieList>(
+        "/movie/upcoming",
+        fetcher
+    );
 
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                overflowX: 'auto',
-                gap: 2,
-            }}
-        >
-            {data.results.map((movie) => (
-                <Grid item xs={12} sm={6} md={4} lg={3} key={movie.id}>
-                    <Link href={`/movie-detail/${movie.id}`} underline="none">
-                        <Card elevation={5} className="zoom-card small-card" sx={{ height: '95%', width: '290%' }}>
-                            <CardMedia
-                                component="img"
-                                sx={{
-                                    height: '100%',
-                                    objectFit: 'cover',
-                                    width: 'auto',
-                                }}
-                                image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                                alt={movie.title}
-                            />
-                            <CardContent></CardContent>
-                        </Card>
-                    </Link>
-                </Grid>
-            ))}
-        </Box>
+        <>
+            <Box sx={{
+                display: "flex",
+                alignItems: "center",
+                flexGrow: 1,
+                width: "317px",
+                color: "#FFF",
+                fontSize: "20px",
+                fontWeight: 600,
+                lineHeight: "normal"
+            }}>
+                What do you want to watch?
+            </Box>
+            <Box sx={{ height: "21px" }}>
+
+            </Box >
+            <Box
+                sx={{
+                    display: "flex",
+                    overflowX: "auto",
+                    gap: 2,
+                    flexWrap: "nowrap",
+                }}
+            >
+                {isLoading && <CircularProgress />}
+                {error && <Typography>Error loading data</Typography>}
+                {data?.results.map((movie) => (
+                    <Box
+                        key={movie.id}
+                        sx={{
+                            flex: "0 0 auto",
+                            marginRight: 2,
+                        }}
+                    >
+                        <Link href={`/movie-detail/${movie.id}`} underline="none">
+                            <Card elevation={5} className="zoom-card small-card" sx={{ height: "210px", width: "139.581px", borderRadius: "16px" }}>
+                                <CardMedia
+                                    component="img"
+                                    sx={{
+                                        height: "100%",
+                                        objectFit: "cover",
+                                        width: "100%",
+                                        borderRadius: "16px",
+                                    }}
+                                    image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                    alt={movie.title}
+                                />
+                                <CardContent />
+                            </Card>
+                        </Link>
+                    </Box>
+                ))}
+            </Box>
+        </>
     );
 };
 
-HomeDetail.getLayout = function getLayout(page) {
+HomeDetail.getLayout = function getLayout(page: ReactElement) {
     return <Layout>{page}</Layout>;
 };
 
