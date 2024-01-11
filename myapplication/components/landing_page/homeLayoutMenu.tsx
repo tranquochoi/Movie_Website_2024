5926e1691b577f8ec79897d64315da51a78a626d
DESKTOP-UF62SM0\WalterWhite <boemhut113@gmail.com>
2024-01-11 13:35:21 +0700

    Update home page

---
+++ myapplication/components/landing_page/homeLayoutMenu.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+import {
+  AppBar,
+  Box,
+  Button,
+  MenuItem,
+  Stack,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import Link from "next/link";
+import { title } from "process";
+
+interface MenuItemObject {
+  title: string;
+  url: string;
+}
+export default function HomeMenu({}: {}) {
+  const menu: MenuItemObject[] = [
+    { title: "Now playing", url: "/home/listMenu/now" },
+    { title: "Up comming", url: "/home/listMenu/upComing" },
+    { title: "Top rated", url: "/home/listMenu/topRated" },
+    { title: "Popular", url: "/home/listMenu/popular" },
+  ];
+
+  const [currentMenu, setCurrentMenu] = useState(0);
+
+  return (
+    <Box>
+      <Toolbar>
+        <Stack direction="row" gap={4}>
+          {menu.map((item, index) => (
+            <Link
+              key={item.url}
+              href={item.url}
+              style={{ color: `${currentMenu == index ? "red" : "gray"}` }}
+              onClick={() => setCurrentMenu(index)}
+            >
+              {item.title}
+            </Link>
+          ))}
+        </Stack>
+      </Toolbar>
+
+      <Toolbar />
+    </Box>
+  );
+}
