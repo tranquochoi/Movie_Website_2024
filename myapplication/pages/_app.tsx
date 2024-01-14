7c402ae4e0e1797cfb66bc724efa94696c1f5e0e
DESKTOP-UF62SM0\WalterWhite <boemhut113@gmail.com>
2024-01-14 17:09:33 +0700

    Thay đổi apptsx, thêm configts

--- myapplication/pages/_app.tsx
+++ myapplication/pages/_app.tsx
@@ -3,6 +3,14 @@ import type { AppProps } from "next/app";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import axios from "axios";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { SWRConfig } from "swr";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Raleway", "san-serif"].join(","),
+  },
+});
 
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
 axios.defaults.headers["Authorization"] =
@@ -17,8 +25,17 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-
   const getLayout = Component.getLayout ?? ((page) => page);
-
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <ThemeProvider theme={theme}>
+      <SWRConfig
+        value={{
+          fetcher: (resource, init) =>
+            axios.get(resource, init).then((res) => res.data),
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
+    </ThemeProvider>
+  );
 }
