1bee1cfca6419598038bdba1953b003ce8a5b008
DESKTOP-UF62SM0\WalterWhite <boemhut113@gmail.com>
2024-01-14 20:13:12 +0700

    Hien tai day nha moi nguoi

--- myapplication/pages/movie-detail/[id].tsx
+++ myapplication/pages/movie-detail/[id].tsx
@@ -1,49 +1,42 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import useSWR from "swr";
-import Layout from '@/components/landing_page/layout';
-import { NextPageWithLayout } from '../_app';
-import NavDetail from '@/components/landing_page/NavDetail';
-import TabDetail from '@/components/landing_page/TabDetail';
+import Layout from "@/components/landing_page/layout";
+import { NextPageWithLayout } from "../_app";
+import NavDetail from "@/components/landing_page/NavDetail";
+import TabDetail from "@/components/landing_page/TabDetail";
 import {
-    Box,
-    Button,
-    Container,
-    Grid,
-    Link,
-    Stack,
-    TextField,
-    Typography
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  Stack,
+  TextField,
+  Typography,
 } from "@mui/material";
-import { Movies } from './Models/Movies';
-import config from '@/config';
+import { Movies } from "./Models/Movies";
+import config from "@/config";
 
 const Detail: NextPageWithLayout = () => {
-    const router = useRouter();
-    const id=router.query.id;
-    const { data, isLoading, error } = useSWR<Movies>(
-        `/movie/${id}?language=en-US&append_to_response=videos,credits`
-      );
-    return (
-        <>
-            <NavDetail />
-            <Box>
-                fgsdf
+  const router = useRouter();
+  const id = router.query.id;
+  const { data, isLoading, error } = useSWR<Movies>(
+    `/movie/${id}?language=en-US&append_to_response=videos,credits`
+  );
+  return (
+    <>
+      <NavDetail />
+      <Box>fgsdf</Box>
 
-            </Box>
-
-            <TabDetail />
-        </>
-    )
-}
+      <TabDetail />
+    </>
+  );
+};
 
 Detail.getLayout = function getLayout(page) {
-    return (
-        <Layout>
-            {page}
-        </Layout>
-    )
-}
+  return <Layout>{page}</Layout>;
+};
 export default Detail;
