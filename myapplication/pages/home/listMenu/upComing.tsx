837c3fe90ff983aea5dca72012954e6b30025612
DESKTOP-UF62SM0\WalterWhite <boemhut113@gmail.com>
2024-01-18 17:09:28 +0700

    Bỏ fetcher không cần thiết

--- myapplication/pages/home/listMenu/upComing.tsx
+++ myapplication/pages/home/listMenu/upComing.tsx
@@ -22,7 +22,7 @@ import RenderMovie from "./renderMovie";
 const UpComing: NextPageWithLayout = () => {
   const fetcher = (url: string) =>
     axios.get(url).then((response) => response.data);
-  const { data, error } = useSWR<MovieList>("/movie/upcoming", fetcher);
+  const { data, error } = useSWR<MovieList>("/movie/upcoming");
 
   if (!data) {
     return <CircularProgress />;
