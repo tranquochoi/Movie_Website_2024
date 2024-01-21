e7e7dee1b6944f0eb6660209bb90e8dba73ccde1
kazuo <92442628+Kazuo01@users.noreply.github.com>
2024-01-21 16:39:32 +0700

    home update

--- myapplication/pages/home/index.tsx
+++ myapplication/pages/home/index.tsx
@@ -22,48 +22,44 @@ import Person from "@/components/Person";
 import { MovieList } from "../movie-detail/Models/Movies";
 
 const HomeDetail: NextPageWithLayout = () => {
-  // Function to fetch data from the API
   const fetcher = (url: string) =>
     axios.get(url).then((response) => response.data);
 
-  // Use SWR hook to fetch data and manage loading and error states
-  const { data, isLoading, error } = useSWR<MovieList>("/movie/upcoming");
+  const { data, isLoading, error } = useSWR<MovieList>("/movie/popular");
 
   return (
     <>
-      {/* Add some empty space at the top */}
       <Box sx={{ height: "21px" }}></Box>
 
-      {/* Render the Header component */}
       <Header />
 
-      {/* Add some empty space with negative margin to adjust layout */}
       <Box sx={{ height: "21px", marginTop: "-8px" }}></Box>
 
-      {/* Render the SearchBar component */}
       <SearchBar />
 
-      {/* Add some empty space with negative margin to adjust layout */}
-      <Box sx={{ height: "38px", marginTop: "-8px" }}></Box>
-
-      {/* Display a horizontal scrollable container for movie cards */}
+      <Box sx={{ height: "20px", marginTop: "-8px" }}></Box>
+      <Box sx={{
+        marginLeft: "6px",
+        height: "38px",
+        display: "flex",
+        color: "#FFF",
+        fontSize: "16px",
+      }}>New Arrival</Box>
+      <Box sx={{ height: "20px", marginTop: "-8px" }} />
       <Box
         sx={{
           display: "flex",
           overflowX: "auto",
           gap: 2,
           flexWrap: "nowrap",
-          margin: "-14px", // Adjust margin to compensate for negative margin in previous elements
+          margin: "-14px",
           paddingLeft: "18px",
         }}
       >
-        {/* Display loading indicator if data is still being fetched */}
         {isLoading && <CircularProgress />}
 
-        {/* Display error message if there's an issue fetching data */}
         {error && <Typography>Error loading data</Typography>}
 
-        {/* Map through movie data and render individual movie cards */}
         {data?.results.map((movie, index) => (
           <Box
             key={movie.id.toString()}
@@ -73,11 +69,8 @@ const HomeDetail: NextPageWithLayout = () => {
               height: "250px",
             }}
           >
-            {/* Create a link to the movie detail page */}
             <Link href={`/movie-detail/${movie.id}`} underline="none">
-              {/* Container for the movie card */}
               <Box sx={{ position: "relative" }}>
-                {/* Display rating and background overlay */}
                 <Box
                   sx={{
                     marginTop: "20px",
@@ -91,13 +84,11 @@ const HomeDetail: NextPageWithLayout = () => {
                     fontSize: "12px",
                   }}
                 >
-                  {/* Display star icon and movie rating */}
                   <StarIcon sx={{ fontSize: 24, color: "orange" }} />
                   {movie.vote_average}
                 </Box>
               </Box>
 
-              {/* Movie card with elevation, size, and rounded corners */}
               <Card
                 elevation={5}
                 className="small-card"
@@ -105,10 +96,9 @@ const HomeDetail: NextPageWithLayout = () => {
                   height: "200px",
                   width: "139.581px",
                   borderRadius: "16px",
-                  overflow: "hidden", // Ensure image corners are rounded
+                  overflow: "hidden",
                 }}
               >
-                {/* Movie poster with specified height, width, and rounded corners */}
                 <CardMedia
                   component="img"
                   sx={{
@@ -116,17 +106,15 @@ const HomeDetail: NextPageWithLayout = () => {
                     objectFit: "cover",
                     width: "100%",
                     borderRadius: "16px",
-                    boxSizing: "border-box", // Ensure no white border around the image
+                    boxSizing: "border-box",
                   }}
-                  loading="lazy" // Optimize image loading
+                  loading="lazy"
                   src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                   alt={movie.title}
                 />
               </Card>
 
-              {/* Position indicator number at the bottom left of the card */}
               <Box sx={{ position: "relative" }}>
-                {/* Display the position number with styling */}
                 <Typography
                   sx={{
                     position: "absolute",
@@ -139,7 +127,6 @@ const HomeDetail: NextPageWithLayout = () => {
                     textShadow: "1px 1px 4px #0296E5",
                   }}
                 >
-                  {/* Display the position index */}
                   {index + 1}
                 </Typography>
               </Box>
@@ -151,7 +138,6 @@ const HomeDetail: NextPageWithLayout = () => {
   );
 };
 
-// Define the layout for the page including the HomeMenu component
 HomeDetail.getLayout = function getLayout(page: ReactElement) {
   return (
     <Layout>
@@ -161,5 +147,4 @@ HomeDetail.getLayout = function getLayout(page: ReactElement) {
   );
 };
 
-// Export the HomeDetail component as the default export
 export default HomeDetail;
