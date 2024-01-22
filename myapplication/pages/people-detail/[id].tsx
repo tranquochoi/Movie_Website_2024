2ee4567d002f0f9df54adb60b1c9143840eb4d3b
MSI\ADMIN <huynhduy8122002@gmail.com>
2024-01-22 17:41:47 +0700

    push detail profile

--- myapplication/pages/people-detail/[id].tsx
+++ myapplication/pages/people-detail/[id].tsx
@@ -1,37 +1,30 @@
-import React from "react";
-import { useRouter } from "next/router";
+import React, { useState } from "react";
 import useSWR from "swr";
-import Layout from "@/components/landing_page/layout";
-import NavDetail from "@/components/landing_page/NavDetail";
-import TabDetail from "@/components/landing_page/TabDetail";
-import StarIcon from "@mui/icons-material/Star";
-import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
+import { useRouter } from "next/router";
 import {
   Box,
   CardMedia,
   CircularProgress,
-  Grid,
   Link,
   Stack,
-  Toolbar,
   Typography,
 } from "@mui/material";
-
 import config from "@/config";
 import { NextPageWithLayout } from "../_app";
-import { green } from "@mui/material/colors";
 import { People } from "../movie-detail/Models/People";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import JobOnIcon from "@mui/icons-material/RecentActors";
-import RenderMovie from "../home/listMenu/renderMovie";
+import NavProfile from "@/components/landing_page/NavProfile";
+
 const PeopleDetail: NextPageWithLayout = () => {
   const router = useRouter();
   const { id } = router.query;
   const { data, isLoading, error } = useSWR<People>(
     `/person/${id}?language=en-US&append_to_response=images,external_ids,movie_credits`
   );
+
+  const [showFullBiography, setShowFullBiography] = useState(false);
+  const [showFullFilmList, setShowFullFilmList] = useState(false);
 
   if (isLoading) {
     return <Typography>{isLoading && <CircularProgress />}</Typography>;
@@ -44,198 +37,235 @@ const PeopleDetail: NextPageWithLayout = () => {
   if (!data) {
     return <>Không có dữ liệu</>;
   }
+
   const sortedListFilm = data.movie_credits.cast.sort(
-    (a, b) =>
-      parseInt(b.release_date.slice(0, 4)) -
-      parseInt(a.release_date.slice(0, 4))
+    (a, b) => parseInt(b.release_date.slice(0, 4)) - parseInt(a.release_date.slice(0, 4))
   );
+
+  // Lấy 20 từ đầu tiên từ tiểu sử
+  const shortBiography = data.biography.split(" ").slice(0, 20).join(" ");
+
   return (
-    <Box sx={{ padding: "18px" }}>
-      <Stack spacing={3} sx={{ alignItems: "center" }}>
-        <Box
-          component="img"
-          sx={{
-            height: "100px",
-            width: "100px",
-            objectFit: "cover",
-            borderRadius: "150px",
-          }}
-          src={config.image_path + data.images.profiles[0].file_path}
-          alt={"none"}
-        />
+    <>
+      <NavProfile />
+      <Box sx={{ padding: "18px" }}>
+        <Stack spacing={3} sx={{ alignItems: "center" }}>
+          <Box
+            component="img"
+            sx={{
+              height: "100px",
+              width: "100px",
+              objectFit: "cover",
+              borderRadius: "150px",
+            }}
+            src={config.image_path + data.images.profiles[0].file_path}
+            alt={"none"}
+          />
 
-        <Typography
-          sx={{
-            color: "#FFF",
-            fontFamily: "Hanuman",
-            fontSize: "18px",
-            fontStyle: "normal",
-            fontWeight: 400,
-            lineHeight: "normal",
-          }}
-        >
-          {data.name}
-        </Typography>
+          <Typography
+            sx={{
+              color: "#FFF",
+              fontFamily: "monospace",
+              fontSize: "30px",
+              fontStyle: "normal",
+              fontWeight: 400,
+              lineHeight: "normal",
+            }}
+          >
+            {data.name}
+          </Typography>
 
-        <Typography
-          sx={{
-            color: "#544C4C",
-            fontFamily: "Inter",
-            fontSize: "14px",
-            fontStyle: "normal",
-            fontWeight: 500,
-            lineHeight: "87.523%",
-          }}
-        >
-          <LocationOnIcon />
-          {data.place_of_birth}
-        </Typography>
-        <Typography
-          sx={{
-            color: "#544C4C",
-            fontFamily: "Inter",
-            fontSize: "14px",
-            fontStyle: "normal",
-            fontWeight: 500,
-            lineHeight: "87.523%",
-          }}
-        >
-          <JobOnIcon />
-          {data.known_for_department}
-        </Typography>
+          <Typography
+            sx={{
+              color: "white",
+              fontFamily: "Inter",
+              fontSize: "20px",
+              fontStyle: "normal",
+              fontWeight: 500,
+              lineHeight: "87.523%",
+            }}
+          >
+            <LocationOnIcon />
+            {data.place_of_birth}
+          </Typography>
+          <Typography
+            sx={{
+              color: "white",
+              fontFamily: "Inter",
+              fontSize: "20px",
+              fontStyle: "normal",
+              fontWeight: 500,
+              lineHeight: "87.523%",
+            }}
+          >
+            <JobOnIcon />
+            {data.known_for_department}
+          </Typography>
 
-        <Stack direction="row" spacing={8}>
-          <Box>
-            <Typography
-              sx={{
-                color: "#242760",
-                fontFamily: "Inter",
-                fontSize: "24px",
-                fontStyle: "normal",
-                fontWeight: 600,
-                lineHeight: "87.523%",
-              }}
-            >
-              {data.popularity}
-            </Typography>
-            <Typography
-              sx={{
-                color: "#FFF",
-                fontFamily: "Inter",
-                fontSize: "14px",
-                fontStyle: "normal",
-                fontWeight: 500,
-                lineHeight: "87.523%",
-              }}
-            >
-              Popularity
-            </Typography>
-          </Box>
-          <Box>
-            <Typography
-              sx={{
-                color: "#242760",
-                fontFamily: "Inter",
-                fontSize: "24px",
-                fontStyle: "normal",
-                fontWeight: 600,
-                lineHeight: "87.523%",
-              }}
-            >
-              Twitter
-            </Typography>
-            <Typography
-              sx={{
-                color: "#FFF",
-                fontFamily: "Inter",
-                fontSize: "14px",
-                fontStyle: "normal",
-                fontWeight: 500,
-                lineHeight: "87.523%",
-              }}
-            >
-              @{data.external_ids.twitter_id}
-            </Typography>
-          </Box>
+          <Stack direction="row" spacing={8}>
+            <Box>
+              <Typography
+                sx={{
+                  color: "gold",
+                  fontFamily: "Inter",
+                  fontSize: "20px",
+                  fontStyle: "normal",
+                  fontWeight: 600,
+                  lineHeight: "87.523%",
+                }}
+              >
+                Popularity:
+              </Typography>
+              <Typography
+                sx={{
+                  color: "white",
+                  fontFamily: "Inter",
+                  fontSize: "20px",
+                  fontStyle: "normal",
+                  fontWeight: 500,
+                  lineHeight: "87.523%",
+                  textAlign: "center",
+                }}
+              >
+                {data.popularity}
+              </Typography>
+            </Box>
+            <Box>
+              <Typography
+                sx={{
+                  color: "gold",
+                  fontFamily: "Inter",
+                  fontSize: "20px",
+                  fontStyle: "normal",
+                  fontWeight: 600,
+                  lineHeight: "87.523%",
+                }}
+              >
+                Twitter:
+              </Typography>
+              <Typography
+                sx={{
+                  color: "#FFF",
+                  fontFamily: "Inter",
+                  fontSize: "20px",
+                  fontStyle: "normal",
+                  fontWeight: 500,
+                  lineHeight: "87.523%",
+                }}
+              >
+                @{data.external_ids.twitter_id}
+              </Typography>
+            </Box>
+          </Stack>
+
+          <Typography
+            sx={{
+              color: "#FFF",
+              fontFamily: "Inter",
+              fontSize: "20px",
+              fontStyle: "normal",
+              fontWeight: 400,
+              lineHeight: "normal",
+              textAlign: "justify",
+              maxHeight: showFullBiography ? "none" : "100px",
+              overflow: "hidden",
+            }}
+          >
+            {showFullBiography ? data.biography : shortBiography}
+
+            {data.biography.split(" ").length > 20 && (
+              <Link
+                onClick={() => setShowFullBiography(!showFullBiography)}
+                sx={{
+                  textDecoration: "underline",
+                  color: "#00F",
+                  cursor: "pointer",
+                  display: "block",
+                  marginTop: 1,
+                }}
+              >
+                {showFullBiography ? "Read less" : "Read more"}
+              </Link>
+            )}
+          </Typography>
         </Stack>
-
-        <Typography
-          sx={{
-            color: "#FFF",
-            fontFamily: "Inter",
-            fontSize: "20px",
-            fontStyle: "normal",
-            fontWeight: 400,
-            lineHeight: "normal",
-            textAlign: "center",
-          }}
-        >
-          {data.biography}
-        </Typography>
-      </Stack>
-      <Box sx={{}}>
-        <Typography
-          sx={{
-            color: "#242760",
-            fontFamily: "Inter",
-            fontSize: "24px",
-            fontStyle: "normal",
-            fontWeight: 600,
-            lineHeight: "87.523%",
-          }}
-        >
-          Acting for:
-        </Typography>
-        {sortedListFilm.map((ig) => (
-          <Box>
+        <Box sx={{ mt: 3, borderTop: "1px solid #777" }}>
+          <Typography
+            sx={{
+              color: "#242760",
+              fontFamily: "Inter",
+              fontSize: "24px",
+              fontWeight: 600,
+              lineHeight: "87.523%",
+            }}
+          >
+            Acting for:
+          </Typography>
+          {sortedListFilm.slice(0, showFullFilmList ? sortedListFilm.length : 5).map((ig, index) => (
+            <Box key={index}>
+              <Link
+                href={`/movie-detail/${ig.id}`}
+                underline="none"
+                sx={{
+                  textDecoration: "none",
+                  textAlign: "center",
+                  color: "#FFF",
+                  "&:hover": {
+                    color: "#00F",
+                  },
+                }}
+              >
+                <Typography
+                  style={{
+                    fontFamily: "Roboto",
+                    fontSize: "24px",
+                    fontWeight: 700,
+                    lineHeight: "normal",
+                    display: "inline-block",
+                    marginRight: "8px",
+                    maxWidth: "calc(100% - 100px)",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  {ig.release_date && ig.release_date.slice(0, 4)}
+                </Typography>
+                <Typography
+                  style={{
+                    fontFamily: "Roboto",
+                    fontSize: "24px",
+                    fontWeight: 700,
+                    lineHeight: "normal",
+                    display: "inline-block",
+                    maxWidth: "calc(100% - 100px)",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  {ig.title}
+                </Typography>
+              </Link>
+            </Box>
+          ))}
+          {sortedListFilm.length > 5 && (
             <Link
-              href={`/movie-detail/${ig.id}`}
-              underline="none"
+              onClick={() => setShowFullFilmList(!showFullFilmList)}
               sx={{
-                textDecoration: "none",
-                textAlign: "center",
-                color: "#FFF",
-                "&:hover": {
-                  color: "#00F", // Màu xanh khi di chuột vào
-                },
+                textDecoration: "underline",
+                color: "#00F",
+                cursor: "pointer",
+                display: "block",
+                marginTop: 1,
               }}
             >
-              <Typography
-                style={{
-                  fontFamily: "Roboto",
-                  fontSize: "24px",
-                  fontWeight: 700,
-                  lineHeight: "normal",
-                  display: "inline-block",
-                  marginRight: "8px",
-                  maxWidth: "calc(100% - 100px)",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  whiteSpace: "nowrap",
-                }}
-              >
-                {ig.release_date && ig.release_date.slice(0, 4)}
-              </Typography>
-              <Typography
-                style={{
-                  fontFamily: "Roboto",
-                  fontSize: "24px",
-                  fontWeight: 700,
-                  lineHeight: "normal",
-                  display: "inline-block",
-                  maxWidth: "calc(100% - 100px)",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  whiteSpace: "nowrap",
-                }}
-              >
-                {ig.title}
-              </Typography>
+              {showFullFilmList ? "Show less movies" : "Show more movies"}
             </Link>
-          </Box>
-        ))}
+          )}
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
