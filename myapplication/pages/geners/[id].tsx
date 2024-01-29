db62938b358f171732ad773fbc28688ca17e6682
TinNguyen <tin.ntr.62cntt@ntu.edu.vn>
2024-01-30 01:03:11 +0700

    Commit genres ID

--- myapplication/pages/geners/[id].tsx
+++ myapplication/pages/geners/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import axios from "axios";
@@ -15,13 +15,18 @@ import {
 } from "@mui/material";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { NextPageWithLayout } from "../_app";
-import { initializeTraceState } from "next/dist/trace";
+import RenderMovie4 from "../home/listMenu/renderMovie4";
 
 const Categories: NextPageWithLayout = () => {
   const router = useRouter();
   const { id } = router.query;
   const [selectedGenre, setSelectedGenre] = useState(0);
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [movies, setMovies] = useState<MovieList | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage, setPerPage] = useState(10);
+
+  const lastMovieRef = useRef<HTMLDivElement>(null);
 
   const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -38,32 +43,86 @@ const Categories: NextPageWithLayout = () => {
 
   const fetcher = (url: string) =>
     axios.get(url).then((response) => response.data);
-  const { data, isLoading, error } = useSWR<MovieList>(
-    "/movie/now_playing",
-    fetcher
-  );
+
+  const fetchMovies = async () => {
+    try {
+      const response = await axios.get(
+        `/movie/top_rated?page=${currentPage}&per_page=${perPage}`
+      );
+      const newMovies = response.data;
+
+      setMovies((prevMovies) => ({
+        page: newMovies.page,
+        results: prevMovies
+          ? [
+              ...prevMovies.results,
+              ...newMovies.results.filter(
+                (newMovie: { id: Int16Array }) =>
+                  !prevMovies.results.some(
+                    (existingMovie) => existingMovie.id === newMovie.id
+                  )
+              ),
+            ]
+          : newMovies.results,
+      }));
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [currentPage, perPage]);
 
   const { data: gener } = useSWR<ListGenre>(
     "genre/movie/list?language=en&page=3",
     fetcher
   );
-  const movieGener = data?.results.filter((movie) =>
-    movie.genre_ids.includes(
-      selectedGenre != 0 ? selectedGenre : parseInt(id as string)
+
+  const movieGener = movies?.results
+    ?.filter((movie) =>
+      movie.genre_ids.includes(
+        selectedGenre !== 0 ? selectedGenre : parseInt(id as string)
+      )
     )
-  );
+    .filter(
+      (movie, index, self) => index === self.findIndex((m) => m.id === movie.id)
+    );
 
-  if (isLoading) {
-    return <Typography>{isLoading && <CircularProgress />}</Typography>;
+  const handleLoadMore = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+    setPerPage(10); // Set lại số lượng phim muốn hiển thị mỗi lần khi ấn "Load More"
+  };
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1, // Trigger when 10% of the target is visible
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        handleLoadMore();
+      }
+    }, options);
+
+    // Attach the observer to the last movie element
+    if (lastMovieRef.current) {
+      observer.observe(lastMovieRef.current);
+    }
+
+    // Clean up the observer when component unmounts
+    return () => {
+      if (lastMovieRef.current) {
+        observer.unobserve(lastMovieRef.current);
+      }
+    };
+  }, [lastMovieRef.current, handleLoadMore]);
+
+  if (!movies) {
+    return <CircularProgress />;
   }
 
-  if (error) {
-    return <Typography sx={{ color: "red" }}>Error</Typography>;
-  }
-
-  if (!data) {
-    return <>Không có dữ liệu</>;
-  }
+  const maxPopoverHeight = 10 * 20;
 
   return (
     <>
@@ -79,17 +138,9 @@ const Categories: NextPageWithLayout = () => {
           display: "flex",
           flexDirection: "row",
           flexWrap: "wrap",
-          backgroundColor: "",
-          padding: "6px",
+          paddingLeft: "12px",
         }}
       >
-        <Button
-          onClick={handleMenuOpen}
-          sx={{ display: "flex", alignItems: "center", color: "white" }}
-        >
-          Thể loại <KeyboardArrowDownIcon />
-        </Button>
-
         <Popover
           open={Boolean(anchorEl)}
           anchorEl={anchorEl}
@@ -105,46 +156,75 @@ const Categories: NextPageWithLayout = () => {
           PaperProps={{
             style: {
               backgroundColor: "#242A32",
-            },
-          }}
-        >
-          <Box
-            sx={{
+              maxHeight: `${maxPopoverHeight}px`,
+              overflowY: "auto",
               display: "flex",
               flexDirection: "row",
               flexWrap: "wrap",
-              padding: "6px",
-            }}
-          >
-            {gener?.genres.map((genre) => (
-              <Box
-                key={genre.id.toString()}
-                sx={{
-                  border: "2px solid #888",
-                  padding: "4px 8px",
-                  borderRadius: "2px",
-                  marginRight: "8px",
-                  marginTop: "10px",
-                  backgroundColor:
-                    selectedGenre == genre.id ? "#4a92ff" : "transparent",
-                  color: selectedGenre == genre.id ? "white" : "#949494",
-                  "&:hover": {
-                    backgroundColor: "#4a92ff",
-                    cursor: "pointer",
-                  },
-                }}
-                onClick={() => {
-                  setSelectedGenre(genre.id);
-                  handleMenuClose();
-                }}
-              >
-                {genre.name}
-              </Box>
-            ))}
+              justifyContent: "space-between",
+              padding: "16px",
+            },
+          }}
+        >
+          <Box sx={{ width: "calc(50% - 16px)" }}>
+            {gener?.genres
+              .slice(0, Math.ceil(gener.genres.length / 2))
+              .map((genre) => (
+                <Box
+                  key={genre.id.toString()}
+                  sx={{
+                    border: "2px solid #888",
+                    padding: "4px 8px",
+                    borderRadius: "2px",
+                    marginBottom: "8px",
+                    backgroundColor:
+                      selectedGenre == genre.id ? "#0CC2FF95" : "transparent",
+                    color: selectedGenre == genre.id ? "white" : "#fff",
+                    "&:hover": {
+                      backgroundColor: "#333",
+                      cursor: "pointer",
+                    },
+                  }}
+                  onClick={() => {
+                    setSelectedGenre(genre.id);
+                    handleMenuClose();
+                  }}
+                >
+                  {genre.name}
+                </Box>
+              ))}
+          </Box>
+          <Box sx={{ width: "calc(50% - 16px)" }}>
+            {gener?.genres
+              .slice(Math.ceil(gener.genres.length / 2))
+              .map((genre) => (
+                <Box
+                  key={genre.id.toString()}
+                  sx={{
+                    border: "2px solid #888",
+                    padding: "4px 8px",
+                    borderRadius: "2px",
+                    marginBottom: "8px",
+                    backgroundColor:
+                      selectedGenre == genre.id ? "#0CC2FF95" : "transparent",
+                    color: selectedGenre == genre.id ? "white" : "#fff",
+                    "&:hover": {
+                      backgroundColor: "#333",
+                      cursor: "pointer",
+                    },
+                  }}
+                  onClick={() => {
+                    setSelectedGenre(genre.id);
+                    handleMenuClose();
+                  }}
+                >
+                  {genre.name}
+                </Box>
+              ))}
           </Box>
         </Popover>
       </Box>
-      <Typography
+      <Box
         sx={{
           color: "white",
           fontSize: "18px",
@@ -153,7 +233,6 @@ const Categories: NextPageWithLayout = () => {
           backgroundColor: "#161722",
           borderRadius: "0px",
           fontWeight: "bold",
-          fontFamily: "Arial, sans-serif",
         }}
       >
         Selected:
@@ -162,15 +241,53 @@ const Categories: NextPageWithLayout = () => {
             (tl) => tl.id == (selectedGenre != 0 ? selectedGenre : id)
           )?.name
         }
-      </Typography>
-      <Box sx={{ padding: "6px", textAlign: "center" }}>
-        <Grid container spacing={3}>
+      </Box>
+      <Box sx={{ padding: "16px", textAlign: "center" }}>
+        <Grid container spacing={1}>
           {movieGener?.map((movie, index) => (
-            <Grid item xs={4} sm={3} md={4} key={index}>
-              <RenderMovie data={movie} />
+            <Grid item xs={12} sm={4} md={4} key={index}>
+              {/* Assign the ref to the last movie element */}
+              {index === movieGener.length - 1 ? (
+                <div ref={lastMovieRef}>
+                  <RenderMovie4 data={movie} />
+                </div>
+              ) : (
+                <RenderMovie4 data={movie} />
+              )}
             </Grid>
           ))}
         </Grid>
+        {movieGener && movieGener.length > 0 && (
+          <Button
+            onClick={handleLoadMore}
+            style={{
+              color: "white",
+              padding: "8px 16px",
+              margin: "32px",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+              fontSize: "16px",
+              textTransform: "none",
+              fontFamily: "Arial, sans-serif",
+            }}
+          >
+            Loading...
+          </Button>
+        )}
+
+        {!movieGener ||
+          (movieGener.length === 0 && (
+            <Box
+              sx={{
+                color: "white",
+                fontSize: "16px",
+                marginTop: "20px",
+              }}
+            >
+              Nothing to show.
+            </Box>
+          ))}
       </Box>
     </>
   );
