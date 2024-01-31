90281ff8bbf929a347f368452f1a40f5b1ba7140
kazuo <92442628+Kazuo01@users.noreply.github.com>
2024-01-31 22:29:37 +0700

    new commit 2 (final)

--- myapplication/pages/home/listMenu/popular.tsx
+++ myapplication/pages/home/listMenu/popular.tsx
@@ -5,6 +5,8 @@ import { NextPageWithLayout } from "@/pages/_app";
 import RenderMovie2 from "./renderMovie2";
 import { Movie } from "@/components/Models/Movies";
 import RenderMovie3 from "./renderMovie3";
+import RenderMovie from "./renderMovie";
+import RenderMovie4 from "./renderMovie4";
 
 const Popular: NextPageWithLayout = () => {
   const fetcher = (url: string) =>
@@ -27,7 +29,7 @@ const Popular: NextPageWithLayout = () => {
       }}
     >
       {data.results.slice(start, end).map((movie: Movie) => (
-        <RenderMovie3 data={movie} />
+        <RenderMovie2 data={movie} />
       ))}
     </Box>
   );
