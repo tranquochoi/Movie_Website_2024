a6b0efd0a72580d0defcd860d3e88d0d1110e312
DESKTOP-UF62SM0\WalterWhite <boemhut113@gmail.com>
2024-02-19 23:01:11 +0700

    Fix favori user

--- pages/users/Favorite.tsx
+++ pages/users/Favorite.tsx
@@ -17,7 +17,7 @@ import {
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { NextPageWithLayout } from "../_app";
 import RenderMovie2 from "../home/listMenu/renderMovie2";
-import RenderMovie3 from "../home/listMenu/renderMovie3";
+
 import RenderMovie4 from "../home/listMenu/renderMovie4";
 import { getCookie } from "cookies-next";
 import NavProfile from "@/components/landing_page/NavProfile";
@@ -47,17 +47,17 @@ const FavoriteMovie: NextPageWithLayout = () => {
           page: newMovies.page,
           results: prevMovies
             ? [
-              ...prevMovies.results,
-              ...newMovies.results.filter(
-                (newMovie: { id: Number }) =>
-                  !prevMovies.results.some(
-                    (existingMovie) => existingMovie.id === newMovie.id
-                  )
-              ),
-            ]
+                ...prevMovies.results,
+                ...newMovies.results.filter(
+                  (newMovie: { id: Number }) =>
+                    !prevMovies.results.some(
+                      (existingMovie) => existingMovie.id === newMovie.id
+                    )
+                ),
+              ]
             : newMovies.results,
         }));
-      } catch (error) { }
+      } catch (error) {}
     };
 
     fetchMovies();
@@ -130,7 +130,6 @@ const FavoriteMovie: NextPageWithLayout = () => {
             </Box>
           ))}
       </Box>
-
     </>
   );
 };
